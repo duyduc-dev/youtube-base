55c935d10776af9440101330a0350a203f23b555
duyduc2003 <dangduyducdangduyduc@gmail.com>
2022-09-01 13:52:03 +0700

    d

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,13 +5,13 @@ import './sidebar.js';
 const pathName = window.location.pathname;
 
 $(() => {
-	if ($(window).width() <= 1350 || pathName === '/video-detail.html') {
+	if ($(window).width() <= 1350 || pathName === '/youtube-base/video-detail.html') {
 		$('.sidebar.sidebar--active').removeClass('sidebar--active');
 		$('.contain__type').addClass('sidebar--active');
 		$('.contain').addClass('sidebar--active');
 	}
 
-	if (pathName === '/video-detail.html') {
+	if (pathName === '/youtube-base/video-detail.html') {
 		if ($('.sidebar.sidebar--active').is(':visible')) {
 			$('#overlay').show();
 		}
